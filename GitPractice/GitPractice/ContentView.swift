b8ae8a4cd8bcf291cc117cb99d109d0e0b08f5c1
yanghojoon <yanghojoon8487@gmail.com>
2023-12-26 09:45:30 +0900

    chore: Config 파일 추가

--- GitPractice/GitPractice/ContentView.swift
+++ GitPractice/GitPractice/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var secretKey: String {
+        guard let secretKey = Bundle.main.infoDictionary?["SecretKey"] as? String else {
+            return ""
+        }
+        
+        return secretKey
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            Text("안녕하세요")
+            Text("시크릿 키: \(secretKey)")
         }
         .padding()
     }
